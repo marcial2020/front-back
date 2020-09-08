033100efc418169f755138bf466410f818d26d60
marcial melong <s1270701@student.mcckc.edu>
2020-09-08 00:34:42 -0500

    add my first commit

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+  products: [
+    {
+      _id: "1",
+      name: "Samsung Galaxy Note 9",
+      category: "Cellphone",
+      image: "/images/samsung note9.jpg",
+      price: 300,
+      brand: "Samsung",
+      rating: 4.5,
+      numReviews: 1000,
+    },
+    {
+      _id: "2",
+      name: "Samsung Galaxy s7",
+      category: "Cellphone",
+      image: "/images/samsung s7.jpg",
+      price: 90,
+      brand: "Samsung",
+      rating: 4.1,
+      numReviews: 3000,
+    },
+    {
+      _id: "3",
+      name: "PowerBank",
+      category: "accessories",
+      image: "/images/powerbank.jpg",
+      price: 25,
+      brand: "Unbrand",
+      rating: 3.5,
+      numReviews: 200,
+    },
+  ],
+};
